998233bb621fd79c6c9e9c121555e89976b38b85
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-04 20:58:11 +0900

    STUDY: sorting an array

---
+++ 01_array/sorting_an_array/sorting_an_array.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int>& arr) {
+    sort(arr.begin(), arr.end());
+    return arr;
+}
+
+int main()
+{
+    int num;
+
+    cin >> num;
+    vector<int> arr(num);
+
+    for (int i = 0; i < num; ++i)
+    {
+        int temp;
+        cin >> temp;
+        arr[i] = temp;
+    }
+
+    solution(arr);
+
+    for (const auto &it : arr)
+    {
+        cout << it << " ";
+    }
+
+    return 0;
+}
