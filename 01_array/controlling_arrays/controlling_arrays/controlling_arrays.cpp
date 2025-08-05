f5e74402b30f8a31659c0d078fa7b04decdba26a
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-05 20:30:23 +0900

    STUDY: controlling arrays

---
+++ 01_array/controlling_arrays/controlling_arrays/controlling_arrays.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> &lst);
+
+int main()
+{
+    int count;
+    cin >> count;
+    vector<int> lst(count);
+
+    int temp;
+    for (int i = 0; i < count; ++i)
+    {
+        cin >> temp;
+        lst[i] = temp;
+    }
+
+    solution(lst);
+
+    for (int it : lst) {
+        cout << it;
+    }
+}
+
+vector<int> solution(vector<int> &lst) {
+    sort(lst.begin(), lst.end(), greater<int>());
+    lst.erase(unique(lst.begin(), lst.end()), lst.end());
+    return lst;
+}
