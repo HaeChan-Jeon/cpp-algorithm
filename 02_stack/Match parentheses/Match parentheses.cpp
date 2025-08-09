a2e5d576fd1fc9679284acff143323fecca19bb4
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-10 08:53:30 +0900

    STUDY: Match parentheses

---
+++ 02_stack/Match parentheses/Match parentheses.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s);
+
+int main()
+{
+    string s = "(())()";
+
+    bool result = solution(s);
+
+    cout << result;
+
+    return 0;
+}
+
+bool solution(string s)
+{
+    stack<bool> stk;
+
+    for (char temp : s)
+    {
+        if (temp == ')')
+        {
+            if (stk.empty())
+            {
+                return false;
+            }
+            else
+            {
+                stk.pop();
+            }
+        }
+        else
+        {
+            stk.push(true);
+        }
+    }
+
+    if (!stk.empty())
+    {
+        return false;
+    }
+
+    return true;
+}
