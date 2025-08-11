8145c0bd4ba228925b002b487e4dac36a5910983
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-11 20:25:27 +0900

    STUDY: Remove in pair

---
+++ 02_stack/Remove in pairs/Remove in pairs.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int solution(string s)
+{
+	stack<char> stk;
+
+	for (char c : s)
+	{
+		if (!stk.empty() && stk.top() == c)
+		{
+			stk.pop();
+		}
+		else
+		{
+			stk.push(c);
+		}
+	}
+
+	return stk.empty();
+}
+
+int main()
+{
+	string s = "baabaa";
+	cout << solution(s) << endl;
+
+	s = "cdcd";
+	cout << solution(s);
+
+	return 0;
+}
