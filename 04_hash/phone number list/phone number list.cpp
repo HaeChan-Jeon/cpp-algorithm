e0f76a8aa8362e7782b21469420fed5777732f3d
HaeChan Jeon <haechan.jeon.memoir@gmail.com>
2025-08-26 19:43:17 +0900

    STUDY: phone number list

---
+++ 04_hash/phone number list/phone number list.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phoneBook)
+{
+    sort(phoneBook.begin(), phoneBook.end());
+
+    for (int i = 0; i < phoneBook.size() - 1; i++)
+    {
+        if (phoneBook[i] == phoneBook[i + 1].substr(0, phoneBook[i].size()))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(void)
+{
+    vector<string> phoneBook;
+
+    phoneBook = { "119", "97674223", "1195524421" };
+
+    cout << solution(phoneBook);
+
+    return 0;
+}
